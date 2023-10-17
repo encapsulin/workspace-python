6c41ad93007f681a1552a66f2923f74b449d71a9
encapsulin <encapsulin@gmail.com>
2023-10-17 13:59:22 +0200

    test

--- screen/screen.py.sh
+++ screen/screen.py.sh
@@ -1,9 +1,11 @@
 while true; do
 python3 screen.py
+
 mDate="$(date +%Y-%m-%d)"
+mkdir ./$mDate
 mTime="$(date +%H-%M-%S)"
 echo $mTime
-mkdir ./$mDate
+
 mv ./screen.png ./$mDate/$mTime.png
 sleep 5
 done
